10ab42f351f7cae9ecc811905ed5ad2255efba09
Arthur-Barreto <arthurbarreto2905@gmail.com>
2021-04-26 11:12:26 -0300

    exemplo de terminal com cor usando a biblioteca bcolors

---
+++ bcolors_exemplo.py
@@ -0,0 +1,19 @@
+''' 
+
+este código foi retirado do seguinte site:
+https://stackoverflow.com/questions/287871/how-to-print-colored-text-to-the-terminal
+apenas um exemplo de como usar a biblioteca bcolors
+
+'''
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+print(bcolors.WARNING + "Warning: No active frommets remain. Continue?" + bcolors.WARNING)
