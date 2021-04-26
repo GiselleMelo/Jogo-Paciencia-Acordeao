e53c6ff32b50929cb43f920f80034caeca54a76c
Arthur-Barreto <arthurbarreto2905@gmail.com>
2021-04-26 17:37:41 -0300

    agr ele retorna o baralho embaralhado usando a biblioteca randit

--- Cria_baralho.py
+++ Cria_baralho.py
@@ -1,3 +1,5 @@
+import random 
+
 def cria_baralho ():
     naipe = ['♠','♥','♦','♣']
     num = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
@@ -5,5 +7,10 @@ def cria_baralho ():
     for n in range(0,len(num)):
         for i in range(0,len(naipe)):
             baralho.append(num[n]+naipe[i])
+    
+    random.shuffle(baralho)
     return baralho
+
+b = cria_baralho()
+print(b)
 
