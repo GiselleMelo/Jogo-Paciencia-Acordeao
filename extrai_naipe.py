d3c9035246cfc7d5aed66f0af514dd2203194334
Arthur-Barreto <arthurbarreto2905@gmail.com>
2021-04-26 14:06:25 -0300

    adicionado o arquivo extrai_naipe.py, operando 100%

---
+++ extrai_naipe.py
@@ -0,0 +1,33 @@
+''' recebe uma str do tipo
+    4♠
+    numero acompanhado do tipo da carta
+    função deve retornar o naipe da carta
+'''
+
+def extrai_naipe(carta):
+# a carta pode ter 3 caracteres, isso precisa ser levado em conta 
+# só queremos o nipe, o último caractere
+# dividindo em dois casos, temos:
+
+    if (len(carta) == 2):
+        # carta do tipo 5♠, com dois caracteres
+        naipe = carta[1]
+        if naipe == "♠":
+            return "♠"
+        elif naipe == "♥":
+            return "♥"
+        elif naipe == "♦":
+            return "♦" 
+        elif naipe == "♣":
+            return "♣"
+    elif (len(carta) == 3):
+        # carta do tipo 10♠, com 3 caracteres
+        naipe = carta[2]
+        if naipe == "♠":
+            return "♠"
+        elif naipe == "♥":
+            return "♥"
+        elif naipe == "♦":
+            return "♦" 
+        elif naipe == "♣":
+            return "♣"
