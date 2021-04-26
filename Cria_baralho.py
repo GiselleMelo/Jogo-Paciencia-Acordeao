c7de6010e3cc4f2d1c753f24871af6f5131bb661
Arthur-Barreto <arthurbarreto2905@gmail.com>
2021-04-26 10:20:23 -0300

    precisa otimizar a função de criar baralho, está tudo manual

--- Cria_baralho.py
+++ Cria_baralho.py
@@ -1,3 +1,11 @@
 def cria_baralho ():
     cartas_baralho = ['A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠','A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦','A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣']
     return cartas_baralho
+
+mensagem = cria_baralho()
+for i in range(0,len(mensagem)):
+    print("A carta de indice {} é {}".format(i,mensagem[i]))
+
+'''
+precisa otimizar essa função do baralho, ta fazendo tudo na mão
+'''
