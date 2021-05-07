f240bfcdae81acb9c9b43cfc23fe453efceb73e7
GiselleMelo <gisellevieira4@gmail.com>
2021-05-07 09:00:41 -0300

    perguntas iniciais

--- pergunta_valor.py
+++ pergunta_valor.py
@@ -8,9 +8,30 @@ from lista_movimentos_possiveis import *
 from Empilha_Carta import *
 from Possui_movimentos_possiveis import *
 from print_baralho import *
+#importa a biblioteca de cores
+from colorama import Fore, Back, Style
 
 # temos 52 cartas, vamos criar um contador para isso, e a medida que 
 # jogamos decrementamos o contador
+
+# iniciando o jogo com as perguntas
+print('Paciência Acordeão') 
+print ('================== ')
+print('Seja bem-vindo(a) ao jogo de Paciência Acordeão! O objetivo deste jogo é colocar todas as cartas em uma mesma pilha.')
+
+print('Existem apenas dois movimentos possíveis:')
+
+print('1. Empilhar uma carta sobre a carta imediatamente anterior;')
+print('2. Empilhar uma carta sobre a terceira carta anterior.1) 
+
+print('Para que um movimento possa ser realizado basta que uma das duas condições abaixo seja atendida:')
+
+print('1. As duas cartas possuem o mesmo valor ou ')
+print('2. As duas cartas possuem o mesmo naipe.')
+
+print('Desde que alguma das condições acima seja satisfeita, qualquer carta pode ser movimentada.')
+
+print('Aperte [Enter] para iniciar o jogo...')
 
 # vamos criar um loop para terminar o jogo ou começa-lo de novo
 desejo = input(("Digite 's' para começar um jogo, para sair do jogo qualquer outra tecla "))
