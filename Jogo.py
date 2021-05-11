1ac913c34ef2d258ab14679e068f4a5e3a9f1346
GiselleMelo <gisellevieira4@gmail.com>
2021-05-11 10:05:41 -0300

    remudando

--- Jogo.py
+++ Jogo.py
@@ -10,27 +10,27 @@ from Possui_movimentos_possiveis import *
 from print_baralho import *
 from direcionamento_escolha import *
 #importa a biblioteca de cores
-from colorama import Fore, Back, Style
+#from colorama import Fore, Back, Style
 
 # temos 52 cartas, vamos criar um contador para isso, e a medida que 
 # jogamos decrementamos o contador
 
 # iniciando o jogo com as perguntas
-print('Paciência Acordeão') 
+print('\nPaciência Acordeão') 
 print ('================== ')
-print('Seja bem-vindo(a) ao jogo de Paciência Acordeão! O objetivo deste jogo é colocar todas as cartas em uma mesma pilha.')
+print('\nSeja bem-vindo(a) ao jogo de Paciência Acordeão! O objetivo deste jogo é colocar todas as cartas em uma mesma pilha.')
 
 print('Existem apenas dois movimentos possíveis:')
 
-print('1. Empilhar uma carta sobre a carta imediatamente anterior;')
+print('\n1. Empilhar uma carta sobre a carta imediatamente anterior;')
 print('2. Empilhar uma carta sobre a terceira carta anterior.') 
 
-print('Para que um movimento possa ser realizado basta que uma das duas condições abaixo seja atendida:')
+print('\nPara que um movimento possa ser realizado basta que uma das duas condições abaixo seja atendida:')
 
-print('1. As duas cartas possuem o mesmo valor ou ')
+print('\n1. As duas cartas possuem o mesmo valor ou ')
 print('2. As duas cartas possuem o mesmo naipe.')
 
-print('Desde que alguma das condições acima seja satisfeita, qualquer carta pode ser movimentada.')
+print('\nDesde que alguma das condições acima seja satisfeita, qualquer carta pode ser movimentada.')
 
 # vamos criar um loop para terminar o jogo ou começa-lo de novo
 desejo = input("Aperte [Enter] para iniciar o jogo... ")
